a621e4ceb963e7902c0204ef281749a12bce62f3
Gilles Cornu <git@gilles.cornu.name>
2015-01-27 08:07:39 +0100

    Test PostGIS on PostgreSQL 9.4 / Python

---
+++ init_postgis.sql
@@ -0,0 +1,7 @@
+
+CREATE EXTENSION postgis;
+CREATE EXTENSION postgis_topology;
+CREATE EXTENSION fuzzystrmatch;
+CREATE EXTENSION postgis_tiger_geocoder;
+SELECT PostGIS_version();
+-- SELECT PostGIS_full_version();
